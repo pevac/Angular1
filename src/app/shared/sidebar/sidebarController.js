db36b48642bd62c79b334455437ee40d7fd570c5
Печенюк Валерий <home.peva@gmail.com>
2016-11-29 18:02:25 +0200

    added drop down

--- src/app/shared/sidebar/sidebarController.js
+++ src/app/shared/sidebar/sidebarController.js
@@ -1,3 +1,22 @@
-angular.module("angularApp").controller("SidebarController", function () {
-    
+angular.module("angularApp").controller("SidebarController", function ($scope) {
+
+    $scope.status = {
+        isopen1: false,
+        isopen2: false
+    };
+
+    $scope.toggled1 = function(open) {
+        $scope.status.isopen1 = !open;
+    };
+    $scope.toggled2 = function(open) {
+        $scope.status.isopen2 = !open;
+    };
+
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+
+    $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
 })
