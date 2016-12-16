2a0bed3d290c4d05668be4de903f63282fdf8b3e
Печенюк Валерий <home.peva@gmail.com>
2016-12-16 17:59:51 +0200

    commit

--- src/app/app.routes.js
+++ src/app/app.routes.js
@@ -27,49 +27,111 @@ angular.module("angularApp").config(function($stateProvider, $urlRouterProvider,
         })
         .state("home.devportfolio", {
             url: "devportfolio",
-            controller: 'devPortfolioCtrl',
-            templateUrl:"devstudio/devportfolio/devportfolio.tmpl.html"
+            template:'<div ng-app="devPortfolioModule"  ui-view ></div>'
         })
+        .state("home.devportfolio.list", {
+            url: "/list",
+            controller: 'devPortfolioListCtrl',
+            templateUrl:"devstudio/devportfolio/list-devportfolio/list-devportfolio.tmpl.html"
+        })
+        .state("home.devportfolio.addportfolio", {
+            url: "/add",
+            controller: "addDevPortfolioCtrl",
+            templateUrl:"devstudio/devportfolio/form-devportfolio/form-devportfolio.tmpl.html"
+        })
+        .state("home.devportfolio.editportfolio", {
+            url: "/edit?project",
+            controller: "addDevPortfolioCtrl",
+            templateUrl:"devstudio/devportfolio/form-devportfolio/form-devportfolio.tmpl.html"
+        })
+
         .state("home.ordercustomer", {
             url: "ordercustomer",
-            templateUrl:"devstudio/order/orderCustomers.tmpl.html",
-            controller: "orderCustomerCtrl"
-            
+            template:'<div ng-app="orderCustomerModule"  ui-view ></div>'
         })
+        .state("home.ordercustomer.list", {
+            url: "/list",
+            templateUrl:"devstudio/order/list-order/order-customers.tmpl.html",
+            controller: "orderCustomerCtrl"
+        })
+        .state("home.ordercustomer.view", {
+            url: "/view?orderId",
+            templateUrl:"devstudio/order/view-order/view-order.tpml.html",
+            controller: "viewOrderCustomerCtrl"
+        })
+
         .state("home.vacancies", {
             url: "vacancies",
-            templateUrl:"devstudio/vacancies/vacancies.tmpl.html",
-
+            template:'<div ng-app="vacancyModule"  ui-view ></div>'
         })
+        .state("home.vacancies.list", {
+            url: "/list",
+            templateUrl:"devstudio/vacancies/list-vacancies/vacancies.tmpl.html",
+            controller: "vacanciesController"
+        })
+        .state("home.vacancies.addvacancy", {
+            url: "/add",
+            templateUrl:"devstudio/vacancies/add-vacancy/addvacancy.tmpl.html",
+            controller: "addVacancyCtrl"
+        })
+        .state("home.vacancies.edit", {
+            url: "/edit?vacancy",
+            templateUrl:"devstudio/vacancies/add-vacancy/addvacancy.tmpl.html",
+            controller: "addVacancyCtrl"
+        })
+        .state("home.vacancies.review", {
+            url: "/review",
+            templateUrl:"devstudio/vacancies/review-vacancies/review-vacancies.tmpl.html",
+            controller: "reviewVacanciesController"
+        })
+
         .state("home.intportfolio", {
             url: "internship-portfolio",
-            templateUrl:"internship/portfolio/intPortfolio.tmpl.html",
-            controller: 'intPortfolioCtrl'
+            template:'<div ng-app="intPortfolioModule"  ui-view ></div>'
         })
+        .state("home.intportfolio.list", {
+            url: "/list",
+            controller: 'intPortfolioListCtrl',
+            templateUrl:"internship/intportfolio/list-intportfolio/list-intportfolio.tmpl.html"
+        })
+        .state("home.intportfolio.addportfolio", {
+            url: "/add",
+            controller: "addIntPortfolioCtrl",
+            templateUrl:"internship/intportfolio/form-intportfolio/form-intportfolio.tmpl.html"
+        })
+        .state("home.intportfolio.editportfolio", {
+            url: "/edit?project",
+            controller: "addIntPortfolioCtrl",
+            templateUrl:"internship/intportfolio/form-intportfolio/form-intportfolio.tmpl.html"
+        })
+
+
         .state("home.reviews", {
             url: "reviews",
-            templateUrl:"internship/reviews/reviews.html"
+            template:'<div ng-app="reviewsModule"  ui-view ></div>'
         })
+        .state("home.reviews.list", {
+            url: "/list",
+            templateUrl:"internship/reviews/listreview/reviews.html"
+        })
+        .state("home.reviews.addreview", {
+            url: "/add",
+            templateUrl:"internship/reviews/add-review/addreview.tmpl.html"
+        })
+        .state("home.reviews.editreview", {
+            url: "/edit?review",
+            templateUrl:"internship/reviews/add-review/addreview.tmpl.html"
+        })
+
+
         .state("home.training", {
             url: "training",
             templateUrl:"internship/training/training.html"
         })
-        .state("home.addvacancy", {
-            url: "addvacancy",
-            templateUrl:"devstudio/addVacancy/addVacancy.tmpl.html"
-        })
-        .state("home.addreview", {
-            url: "addreview",
-            templateUrl:"internship/addReview/addreview.tmpl.html"
-        })
-        .state("home.addportfoliodev", {
-            url: "addportfoliodev",
-            templateUrl:"devstudio/devportfolio/addportfolio.tmpl.html"
-        })
-        .state("home.addportfolioint", {
-            url: "addportfolioint",
-            templateUrl:"internship/portfolio/intAddportfolio.tmpl.html"
-        })
-   
+
+
+
+
+
 });
 
